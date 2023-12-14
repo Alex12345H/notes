cbe13d715e0cf5342b731d8d049fc3ebf4bc5b1e
Alex12345H <152187598+Alex12345H@users.noreply.github.com>
2023-12-14 15:47:17 +0100

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,1 +1,44 @@
+let notes = [];
+
+function addNote() {
+  let noteInput = document.getElementById('noteInput');
+  let note = noteInput.value.trim();
+
+  if (note !== '') {
+    notes.push({ content: note, done: false });
+    noteInput.value = '';
+  }
+}
+
+function showNotes() {
+  let notesOutput = document.getElementById('notesOutput');
+  notesOutput.innerHTML = '';
+
+  if (notes.length === 0) {
+    notesOutput.innerHTML = 'Keine Notizen vorhanden.';
+  } else {
+    for (let i = 0; i < notes.length; i++) {
+      let note = notes[i];
+      let checkbox = note.done ? 'checked' : '';
+      notesOutput.innerHTML += `<div>
+                                  <input type="checkbox" id="checkbox${i}" ${checkbox}>
+                                  <label for="checkbox${i}">${note.content}</label>
+                                </div>`;
+    }
+  }
+}
+
+function deleteNotes() {
+  let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  let updatedNotes = [];
+
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (!checkboxes[i].checked) {
+      updatedNotes.push(notes[i]);
+    }
+  }
+
+  notes = updatedNotes;
+  showNotes();
+}
 
